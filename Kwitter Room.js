a71c5bc2eb9effa0e819b9baf711c332b4503ad3
JoeyTheHarryPotter <74834315+JoeyTheHarryPotter@users.noreply.github.com>
2021-03-14 17:30:57 -0400

    Add files via upload

---
+++ Kwitter Room.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyCwVI-B3n1xUtaO2RxAPtWUoYQPprRC4-0",
+    authDomain: "kwitter-project-895ee.firebaseapp.com",
+    databaseURL: "https://kwitter-project-895ee-default-rtdb.firebaseio.com/",
+    projectId: "kwitter-project-895ee",
+    storageBucket: "kwitter-project-895ee.appspot.com",
+    messagingSenderId: "496569511056",
+    appId: "1:496569511056:web:efa62e3291533a3aa3bc15"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
